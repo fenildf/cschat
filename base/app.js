7deabbf07f843687850c5acf93081df00f997d35
csxiaoyaojianxian <1724338257@qq.com>
2017-09-08 16:12:55 +0800

    基本前端模板完成

---
+++ base/app.js
@@ -0,0 +1,44 @@
+/*
+* @Author: victorsun
+* @Date:   2017-09-08 09:56:26
+* @Last Modified by:   victorsun
+* @Last Modified time: 2017-09-08 16:09:14
+*/
+
+// 滚动条隐藏
+var child = $("#pc .container-fluid .content");
+var ul = $("#pc .container-fluid .content ul");
+child.css("marginRight","-"+(child.width()-ul.width())+"px");
+
+// 提交按钮
+$("#pc button:nth-child(1)").click(function(){
+	addTo($(".userInput").val());
+	addFrom();
+});
+
+// 前端添加用户发送消息内容
+function addTo(val){
+	if(!val){
+		alert("输入内容不能为空!");
+		return;
+	}
+	var content = '<li class="to">'+ val +
+				'<img src="avatar.jpg" alt="avatar"></li>';
+	// 追加内容
+	$("#pc .container-fluid>div:nth-child(2) ul").append(content);
+	// 滚动到顶部
+	var ele = $("#pc .container-fluid>div:nth-child(2)");
+	ele[0].scrollTop = ele[0].scrollHeight;
+	// 清空输入框
+	$(".userInput").val("");
+}
+// 前端添加小智发送消息内容
+function addFrom(val){
+	val = val? val : "系统出故障了哦!";
+	var content = '<li class="from">' + val + '</li>';
+	// 追加内容
+	$("#pc .container-fluid>div:nth-child(2) ul").append(content);
+	// 滚动到顶部
+	var ele = $("#pc .container-fluid>div:nth-child(2)");
+	ele[0].scrollTop = ele[0].scrollHeight;
+}
