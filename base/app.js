d91b1d62b262235076888cc210931f8ebb2cda00
csxiaoyao <1724338257@qq.com>
2017-09-09 13:43:21 +0800

    小智智能客服

--- base/app.js
+++ base/app.js
@@ -4,54 +4,158 @@
 * @Last Modified by:   victorsun
 * @Last Modified time: 2017-09-08 16:25:42
 */
+/**
+ * 初始化信息
+ */
+var avatar = "avatar.jpg";
+// 初始化消息内容
+function initMsg(){
+	addFrom("CS逍遥剑仙，你好呀，我是小智智能客服");
+	addFrom("上个月你一共参加了20场排位赛，胜率80%，在好友中排名第一，再接再厉哦！");
+	addTo("请教对局技巧",avatar);
+	addSysMsg("小智很厉害的哦~~~");
+	addFrom("我们为您准备了一份秘籍……");
+	addTo("我想知道我的好友的战绩",avatar);
+	addFrom("<a href='http://www.csxiaoyao.com'>http://www.csxiaoyao.com</a>");
+	addTo("谢谢啊，你好厉害啊，你叫什么名字呀？",avatar);
+	var top = "欢迎使用小智智能客服，小智智能客服，小智智能客服，小智智能客服";
+	addTopMsg(top);
+	addTopMsg(top);
+}
+
+/**
+ * jQuery elements
+ */
+// 小智头像
+var xzAvatar = $("#pc-img img");
+// 小智会话框
+var dialog = $("#pc");
+// 内容区
+var eleContent = $("#pc .container-fluid>.row:nth-child(2)");
+// 消息列表 ul
+var ul = $("#pc .container-fluid>.row:nth-child(2) ul");
+// 提交按钮
+var btnSubmit =  $("#pc .row:nth-child(3) button:nth-child(1)");
+// 用户输入框
+var inputUser = $(".userInput");
+// 置顶消息框
+// $("#pc .container-fluid>.row:nth-child(2) .alert")
 
 // 滚动条隐藏
-var child = $("#pc .container-fluid .content");
-var ul = $("#pc .container-fluid .content ul");
-child.css("marginRight","-"+(child.width()-ul.width())+"px");
+function hideScroll(){
+	eleContent.css("marginRight","-"+(eleContent.width()-ul.width())+"px");
+}
 
 // 提交按钮
-$("#pc button:nth-child(1)").click(function(){
-	addTo($(".userInput").val());
+btnSubmit.click(function(){
+	addTo(inputUser.val(),avatar);
 	addFrom();
 });
+// 监听回车按键发送消息
+document.onkeydown=function(event){
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(e && e.keyCode==13 && e.target.tagName == "TEXTAREA" && e.target.className == "userInput"){
+		btnSubmit.click();
+	}
+};
+
+// 前端添加置顶消息内容
+function addTopMsg(val){
+	val = val? val : "当前聊天人数过多……";
+	var content = 
+		`<div class="alert alert-warning alert-dismissible pull-left fade in" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+			<strong>置顶消息:</strong> `+ val +`
+		</div>`;
+	// 追加内容
+	eleContent.prepend(content);
+}
 
 // 前端添加用户发送消息内容
-function addTo(val){
+function addTo(val,avatar){
 	if(!val){
 		alert("输入内容不能为空!");
 		return;
 	}
-	var content = '<li class="to">'+ val +
-				'<img src="avatar.jpg" alt="avatar"></li>';
+	var content = '<li class="to">'+ val + '<img src="' + avatar + '" alt="avatar"></li>';
 	// 追加内容
-	$("#pc .container-fluid>div:nth-child(2) ul").append(content);
+	ul.append(content);
 	// 滚动到顶部
-	var ele = $("#pc .container-fluid>div:nth-child(2)");
-	ele[0].scrollTop = ele[0].scrollHeight;
+	scrollToLatest();
 	// 清空输入框
-	$(".userInput").val("");
+	inputUser.val(null);
 }
 // 前端添加小智发送消息内容
 function addFrom(val){
 	val = val? val : "系统出故障了哦!";
 	var content = '<li class="from">' + val + '</li>';
 	// 追加内容
-	$("#pc .container-fluid>div:nth-child(2) ul").append(content);
-	// 滚动到顶部
-	var ele = $("#pc .container-fluid>div:nth-child(2)");
-	ele[0].scrollTop = ele[0].scrollHeight;
+	ul.append(content);
+	// 滚动到最新消息
+	scrollToLatest();
 }
 
+// 前端添加系统消息内容
+function addSysMsg(val){
+	val = val? val : "系统出故障了哦!";
+	var content = '<li class="sys"><strong>系统消息：</strong>' + val + '</li>';
+	// 追加内容
+	ul.append(content);
+	// 滚动到最新消息
+	scrollToLatest();
+}
 
+// 置顶消息对 ul 的位置调整
+function msgAdjust(){
+	var height = 0;
+	var alertContent = $("#pc .container-fluid>.row:nth-child(2)>.alert");
+	alertContent.each(function(key,val){
+		height += val.clientHeight;
+	});
+	ul.animate({"marginTop":height + "px"},500);
+}
 
+// 滚动到最新消息
+function scrollToLatest(){
+	eleContent[0].scrollTop = eleContent[0].scrollHeight;
+}
 
-
-
-/**
- * test
- */
-$("#btn").click(function(){
-	$("#pc").css("display","block");
-	$("#pc").animate({"right":"0"}, 500);
+// 点击alert关闭按钮
+eleContent.on('click', function(event){
+    // 【 事件委托 】
+    var ele = event.target?event.target:event.srcElement;
+    if(ele.tagName === "SPAN" && ele.innerHTML == "×"){
+    	// 置顶消息框
+		$(this).find(".alert").on('closed.bs.alert', function(){
+    		msgAdjust();
+    	});
+    }
 });
+
+$(function(){
+	hideScroll();
+	initMsg();
+	
+	/**
+	 * test
+	 */
+	xzAvatar.click(function(){
+		dialog.css("display","block");
+		dialog.animate({"right":"0"}, 500);
+		msgAdjust();
+	});
+
+});
+
+// 关闭dialog
+$(document).on("click",function (e) {
+	var pop = dialog[0];
+	var avatar = xzAvatar[0];
+	if (e.target!= avatar && e.target!= pop && !$.contains(pop, e.target)){
+		dialog.fadeOut(500,function(){
+			dialog.css("right","-400px");
+		});
+		
+	}
+})
+
