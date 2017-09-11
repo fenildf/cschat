c60d5212996ec00545da11cec88f02c266e1a5a5
csxiaoyaojianxian <1724338257@qq.com>
2017-09-11 15:59:34 +0800

    update

--- app/cschat.js
+++ app/cschat.js
@@ -2,8 +2,53 @@
 * @Author: victorsun
 * @Date:   2017-09-08 09:56:26
 * @Last Modified by:   victorsun
-* @Last Modified time: 2017-09-11 09:43:56
+* @Last Modified time: 2017-09-11 09:48:01
 */
+
+import React, {Component} from 'react';
+// import config from './config.json';
+import './cschat.less';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+class CsChat extends Component{
+  render() {
+    return (
+        // {config.greetText}
+    <div>
+		<div id="pc-img">
+			<img src="xiaozhi01.png" />
+		</div>
+		<div id="pc">
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-xs-2">
+						<img src="xiaozhi02.png" height="46" width="46" alt="" />
+					</div>
+					<div className="col-xs-10">
+						<h4>小智智能客服</h4>
+						<small>颜值爆表，既能萌萌哒，又能。。。</small>
+
+					</div>
+
+				</div>
+				<div className="row"></div>
+				<div className="row">
+					<ul></ul>
+				</div>
+				<div className="row">
+					<div className="col-xs-12"><textarea className="userInput"></textarea></div>
+					<div className="col-xs-8"></div>
+					<div className="col-xs-4"><button className="btn btn-primary">发送</button></div>
+				</div>
+			</div>
+		</div>
+	</div>
+    );
+  }
+}
+
 /**
  * 初始化信息
  */
@@ -206,3 +251,7 @@ function hideScroll(){
 	});
 
 })();
+
+export default CsChat
+
+
--- app/main.js
+++ app/main.js
@@ -2,13 +2,11 @@
 * @Author: victorsun
 * @Date:   2017-09-05 10:03:16
 * @Last Modified by:   victorsun
-* @Last Modified time: 2017-09-05 10:38:20
+* @Last Modified time: 2017-09-11 09:40:47
 */
 //main.js 
 import React from 'react';
 import {render} from 'react-dom';
-import Greeter from './Greeter';
+import CsChat from './cschat';
 
-import './main.css';//使用require导入css文件
-
-render(<Greeter />, document.getElementById('root'));
+render(<CsChat />, document.getElementById('root'));
