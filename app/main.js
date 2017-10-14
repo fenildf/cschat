9fc102df5fb998da94d3e0293ac4de4f914e6ed8
csxiaoyaojianxian <1724338257@qq.com>
2017-10-14 10:51:08 +0800

    main

--- app/main.js
+++ app/main.js
@@ -2,13 +2,19 @@
 * @Author: victorsun
 * @Date:   2017-09-05 10:03:16
 * @Last Modified by:   victorsun
-* @Last Modified time: 2017-09-14 16:47:55
+* @Last Modified time: 2017-10-13 17:50:55
 */
+
+// 浏览器中调用方式
+// cschat("http://www.csxiaoyao.com/src/img/logo.png","1724338257");
+
 import CsChat from './cschat';
 import browserType from './browserType.js';
 
-if(browserType.BrowserType()){
-	let csChat = new CsChat("http://www.csxiaoyao.com/src/img/logo.png");
-}else{
-	alert("浏览器不支持");
+window.cschat = function(avatar,uin){
+	if(browserType.BrowserType()){
+		var csChat = new CsChat(avatar,uin);
+	}else{
+		alert("浏览器不支持");
+	}
 }
